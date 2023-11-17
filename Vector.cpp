5ec0cde31118074b36b71b1e65d1f09c5bacbbd2
Mustafa El-Sherbiny <mostafasherbini114@icloud.com>
2023-11-17 20:59:14 +0200

    Update Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -1,17 +1,5 @@
 #include <bits/stdc++.h>
 //#include <atcoder/all>
-#define f first
-#define s second
-#define FAST()    ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define IN()  freopen("input.txt", "r", stdin);
-#define OUT() freopen("output.txt", "w", stdout) ;
-using namespace std;using ll =  long long ;using ld =  double ;
-const ll  N =2e5+5, M=1e9+7; const double  EPS = 1e-7;
-typedef __int128 i28;
-using vi = vector<ll>;
-template<class T> using min_heap = priority_queue<T, std::vector<T>, std::greater<T>>;
-int dx[] = {0, 0, 1, -1, 1, -1, 1, -1}, dx4[] = {1, 0 , 0 , -1};
-int dy[] = {1, -1, 0, 0, 1, -1, -1, 1}, dy4[] = {0, 1, -1 ,0 };
 
 
 template <typename T>
